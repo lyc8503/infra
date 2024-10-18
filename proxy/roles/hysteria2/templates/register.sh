4549146f19d6383a3dbe49ba4dfceeb46588e2ba
lyc8503 <me@lyc8503.net>
2024-10-19 01:22:41 +0800

    fix: use 1.1.1.1 to get local ip (for easier WARP setup)

--- proxy/roles/hysteria2/templates/register.sh
+++ proxy/roles/hysteria2/templates/register.sh
@@ -1,3 +1,3 @@
-SELF_PUBLIC_IP=$(curl -4 https://cloudflare.com/cdn-cgi/trace | grep 'ip=' | cut -c4-)
+SELF_PUBLIC_IP=$(curl -4 https://1.1.1.1/cdn-cgi/trace | grep 'ip=' | cut -c4-)
 
 curl -G '{{ sub_server }}?token={{ reg_password }}&id={{ sub_id }}_hy2' --data-urlencode "subscription={name: {{ sub_id }}_hy2,type: hysteria2,server: $SELF_PUBLIC_IP,port: {{ hy2_port }},password: {{ v2_uuid }},skip-cert-verify: true,client-fingerprint: chrome}"
