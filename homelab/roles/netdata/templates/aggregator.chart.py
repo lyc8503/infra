fe7e67af4a65f17da92bd372350995e4f4f7ca4f
lyc8503 <me@lyc8503.net>
2024-12-10 21:17:58 +0800

    add temp aggregation

--- homelab/roles/netdata/templates/aggregator.chart.py
+++ homelab/roles/netdata/templates/aggregator.chart.py
@@ -4,14 +4,18 @@ from bases.FrameworkServices.SimpleService import SimpleService
 import requests
 
 def fetch_latest():
-    return requests.get("http://127.0.0.1:19999/api/v1/allmetrics?filter=cgroup*&format=json").json() 
+    r1 = requests.get("http://127.0.0.1:19999/api/v1/allmetrics?filter=cgroup*&format=json").json()
+    r2 = requests.get("http://127.0.0.1:19999/api/v1/allmetrics?filter=*temperature&format=json").json()
+    return r1, r2
 
 NETDATA_UPDATE_EVERY = 1
+TEMPRATURE_WHILELIST = ["smartctl.device_sd", "nvme-pci", "coretemp-isa", "systin"]
 priority = 1000
 
 ORDER = [
     'cgroup_cpu',
-    'cgroup_mem'
+    'cgroup_mem',
+    'temperature'
 ]
 
 CHARTS = {
@@ -23,6 +27,10 @@ CHARTS = {
         'options': ["cgroup_mem", 'Containers & VMs memory usage', 'MiB', 'mem', 'cgroup.mem', 'line'],
         'lines': []
     },
+    'temperature': {
+        'options': ["temperature", 'Temperature', 'Celsius', 'temp', 'temperature', 'line'],
+        'lines': []
+    }
 }
 
 
@@ -38,21 +46,32 @@ class Service(SimpleService):
 
     def get_data(self):
         data = dict()
-        resp = fetch_latest()
+        r1, r2 = fetch_latest()
 
-        for k in resp: 
+        for k in r1: 
             if k.endswith(".cpu"):
                 dim = k.replace("cgroup_", "")
                 if dim not in self.charts['cgroup_cpu']:
                     self.charts['cgroup_cpu'].add_dimension([dim, dim.replace(".cpu", ""), None, None, 100])
 
-                data[dim] = sum(map(lambda x: x["value"] * 100, resp[k]["dimensions"].values()))
+                data[dim] = sum(map(lambda x: x["value"] * 100, r1[k]["dimensions"].values()))
 
             if k.endswith(".mem"):
                 dim = k.replace("cgroup_", "")
                 if dim not in self.charts['cgroup_mem']:
                     self.charts['cgroup_mem'].add_dimension([dim, dim.replace(".mem", ""), None, None, 1000])
                     
-                data[dim] = resp[k]["dimensions"]["anon"]["value"] * 1000
+                data[dim] = r1[k]["dimensions"]["anon"]["value"] * 1000
+
+        for k in r2:
+            if any([i in k for i in TEMPRATURE_WHILELIST]):
+                dim = k.replace("sensors.sensor_chip_", "").replace("smartctl.device_", "")
+                if r2[k]["dimensions"]["temperature"]["value"] is None:
+                    continue
+
+                if dim not in self.charts['temperature']:
+                    self.charts['temperature'].add_dimension([dim, dim, None, None, 1000])
+
+                data[dim] = r2[k]["dimensions"]["temperature"]["value"] * 1000
 
         return data
