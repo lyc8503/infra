6d8aff5e7613c4ffe239e6617db89255b74253ae
lyc8503 <me@lyc8503.net>
2025-01-15 23:25:47 +0800

    ipv6 support for proxy

--- proxy/roles/hysteria2/templates/register.sh
+++ proxy/roles/hysteria2/templates/register.sh
@@ -1,3 +1,9 @@
 SELF_PUBLIC_IP=$(curl -4 https://1.1.1.1/cdn-cgi/trace | grep 'ip=' | cut -c4-)
 
 curl -G '{{ sub_server }}?token={{ reg_password }}&id={{ sub_id }}_hy2&traffic={{ traffic }}' --data-urlencode "subscription={name: {{ sub_id }}_hy2,type: hysteria2,server: $SELF_PUBLIC_IP,port: {{ hy2_port }},password: {{ v2_uuid }},skip-cert-verify: true,client-fingerprint: chrome}"
+
+{% if ipv6_sub %}
+SELF_PUBLIC_IPV6=$(curl -6 https://[2606:4700:4700::1111]/cdn-cgi/trace | grep 'ip=' | cut -c4-)
+
+curl -G '{{ sub_server }}?token={{ reg_password }}&id={{ sub_id }}_v6_hy2&traffic={{ traffic }}' --data-urlencode "subscription={name: {{ sub_id }}_v6_hy2,type: hysteria2,server: $SELF_PUBLIC_IPV6,port: {{ hy2_port }},password: {{ v2_uuid }},skip-cert-verify: true,client-fingerprint: chrome}"
+{% endif %}
