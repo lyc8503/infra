cce4b23f6e879d925e2c2b27bd3a51eb301b2ec6
lyc8503 <me@lyc8503.net>
2025-08-23 15:42:38 +0800

    v6 only proxy

--- proxy/roles/xray/templates/register.sh
+++ proxy/roles/xray/templates/register.sh
@@ -1,8 +1,10 @@
 {% set dest_host, dest_port = v2_reality_dest.split(':') %}
 SELF_PUBLIC_IP=$(curl -4 https://1.1.1.1/cdn-cgi/trace | grep 'ip=' | cut -c4-)
 
-curl -G '{{ sub_server }}?token={{ reg_password }}&id={{ sub_id }}_vision&traffic={{ traffic }}' --data-urlencode "subscription={name: {{ sub_id }}_vision_reality,type: vless,server: $SELF_PUBLIC_IP,port: {{ v2_vision_port }},uuid: {{ v2_uuid }},network: tcp,tls: true,udp: true,flow: xtls-rprx-vision,servername: {{ dest_host }},reality-opts: {public-key: {{v2_reality_pk}},short-id: ae},client-fingerprint: chrome}"
-curl -G '{{ sub_server }}?token={{ reg_password }}&id={{ sub_id }}_grpc&traffic={{ traffic }}' --data-urlencode "subscription={name: {{ sub_id }}_grpc_reality,type: vless,server: $SELF_PUBLIC_IP,port: {{ v2_grpc_port }},uuid: {{ v2_uuid }},network: grpc,tls: true,udp: true,servername: {{ dest_host }},reality-opts: {public-key: {{v2_reality_pk}},short-id: ae},client-fingerprint: chrome,grpc-opts:{grpc-service-name: grpc}}"
+if [ -n "$SELF_PUBLIC_IP" ]; then
+    curl -G '{{ sub_server }}?token={{ reg_password }}&id={{ sub_id }}_vision&traffic={{ traffic }}' --data-urlencode "subscription={name: {{ sub_id }}_vision_reality,type: vless,server: $SELF_PUBLIC_IP,port: {{ v2_vision_port }},uuid: {{ v2_uuid }},network: tcp,tls: true,udp: true,flow: xtls-rprx-vision,servername: {{ dest_host }},reality-opts: {public-key: {{v2_reality_pk}},short-id: ae},client-fingerprint: chrome}"
+    curl -G '{{ sub_server }}?token={{ reg_password }}&id={{ sub_id }}_grpc&traffic={{ traffic }}' --data-urlencode "subscription={name: {{ sub_id }}_grpc_reality,type: vless,server: $SELF_PUBLIC_IP,port: {{ v2_grpc_port }},uuid: {{ v2_uuid }},network: grpc,tls: true,udp: true,servername: {{ dest_host }},reality-opts: {public-key: {{v2_reality_pk}},short-id: ae},client-fingerprint: chrome,grpc-opts:{grpc-service-name: grpc}}"
+fi
 
 {% if ipv6_sub %}
 SELF_PUBLIC_IPV6=$(curl -6 https://[2606:4700:4700::1111]/cdn-cgi/trace | grep 'ip=' | cut -c4-)
