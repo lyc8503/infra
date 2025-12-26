ba067b7ccdec323903f0364e3e11552e1b6cdf66
lyc8503 <me@lyc8503.net>
2025-12-27 00:32:59 +0800

    dn42 try fix tcpdump cleanup

--- dn42/modules/tcpdump.nix
+++ dn42/modules/tcpdump.nix
@@ -1,14 +1,5 @@
 { config, pkgs, lib, ... }:
 
-let
-  cleanupScript = pkgs.writeShellScript "tcpdump-cleanup" ''
-    ${pkgs.findutils}/bin/find /root/dumps -name "capture-*.pcap" -type f -printf '%T@ %p\n' | \
-      ${pkgs.coreutils}/bin/sort -n | \
-      ${pkgs.coreutils}/bin/head -n -10 | \
-      ${pkgs.coreutils}/bin/cut -d' ' -f2- | \
-      ${pkgs.findutils}/bin/xargs -r ${pkgs.coreutils}/bin/rm -f
-  '';
-in
 {
   systemd.services.tcpdump-all = {
     description = "Continuous tcpdump on all interfaces";
@@ -19,11 +10,10 @@ in
       ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p /root/dumps";
       # -i any: Listen on all interfaces
       # -C 256: File size limit 256MB
-      # -G 86400: Rotate at least once a day (required to enable strftime in -w)
-      # -w ...: Output file with timestamp
-      # -z ...: Run cleanup script after rotation
+      # -W 10: Keep 10 files
+      # -w ...: Output file base name
       # -Z root: Run as root
-      ExecStart = "${pkgs.tcpdump}/bin/tcpdump -i any -C 256 -G 86400 -w '/root/dumps/capture-%%Y-%%m-%%d_%%H:%%M:%%S.pcap' -z ${cleanupScript} -Z root";
+      ExecStart = "${pkgs.tcpdump}/bin/tcpdump -i any -C 256 -W 10 -w '/root/dumps/capture.pcap' -Z root 'not port 22 and not port 443'";
       Restart = "always";
       RestartSec = "5s";
     };
