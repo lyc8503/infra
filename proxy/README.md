085a417d577bfeb625c5fcf217e37b1ea1f928a8
lyc8503 <me@lyc8503.net>
2026-01-04 00:08:56 +0800

    not really...

---
+++ proxy/README.md
@@ -0,0 +1,3 @@
+old Ansible config for my VPSes, now most my VPSes are using Nix config and joined DN42.
+
+This is left for VPS with extremely low memory/disk that can't run NixOS.
--- proxy/roles/hysteria2/templates/register.sh
+++ proxy/roles/hysteria2/templates/register.sh
@@ -7,5 +7,5 @@ fi
 {% if ipv6_sub %}
 SELF_PUBLIC_IPV6=$(curl -6 https://[2606:4700:4700::1111]/cdn-cgi/trace | grep 'ip=' | cut -c4-)
 
-curl -G '{{ sub_server }}?token={{ reg_password }}&id={{ sub_id }}_v6_hy2&traffic={{ traffic }}' --data-urlencode "subscription={name: {{ sub_id }}_v6_hy2,type: hysteria2,server: $SELF_PUBLIC_IPV6,port: {{ hy2_port }},password: {{ v2_uuid }},skip-cert-verify: true,client-fingerprint: chrome}"
+curl -G '{{ sub_server }}?token={{ reg_password }}&id={{ sub_id }}_v6_hy2&traffic={{ traffic }}' --data-urlencode "subscription={name: {{ sub_id }}_v6_hy2,type: hysteria2,server: "\""$SELF_PUBLIC_IPV6"\"",port: {{ hy2_port }},password: {{ v2_uuid }},skip-cert-verify: true,client-fingerprint: chrome}"
 {% endif %}
